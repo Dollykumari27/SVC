c637bc435da39ce05d4a4f3886bee8a7578bee83
Dollykumari27 <104142301+Dollykumari27@users.noreply.github.com>
2026-08-25 19:49:00 +0530

    Refactor codebook_gen function for random integer generation

--- codebook_gen.m
+++ codebook_gen.m
@@ -1,16 +1,14 @@
-function [c, mu_max] = codebook_gen(m,N,mu)
+function [c, mu_max] = codebook_gen(m,N)
 
-alpha     = sqrt(2*((2^mu)-1)/3); % normalization factor decided by the modulated symbols
 for i=1:1e3
-    c            = (1/alpha)*(randsrc(m,N));
+    c            = (randi(m,N));
     nf               = sqrt(sum(  c .*conj(  c ),1));   % normalization factor
-    cN               = bsxfun(@rdivide,c,nf);                    % cN is a matrix with normalized columns
-    mucodebook(i)    = max(max(triu(abs(cN'*cN),1)));
+    cN               = bsxfun(@rdivide,c);                    % cN is a matrix with normalized columns
+    mucodebook(i)    = max(max(triu(abs(cN*cN),1)));
     if mucodebook(i) > 0.7 && mucodebook(i) < 0.72
         Cbest        = c;
-        muMIN        = mucodebook(i);
     end %end if
 end %end for
 c         = normc(Cbest );
-mu_max    = max(max(triu(abs(c'*c ),1)));
+mu_max    = max(triu(abs(c'*c ),1)));
 end
